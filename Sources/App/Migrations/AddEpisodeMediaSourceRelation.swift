b8b32b1276a9497522ba8d73d65395a8e1e6e65f
Barna Nemeth <nemethbarna9@gmail.com>
2024-06-04 15:20:27 +0200

    Fix AddEpisodeMediaSourceRelation migration

--- Sources/App/Migrations/AddEpisodeMediaSourceRelation.swift
+++ Sources/App/Migrations/AddEpisodeMediaSourceRelation.swift
@@ -11,8 +11,17 @@ import SQLKit
 
 struct AddEpisodeMediaSourceRelation: AsyncMigration {
     
+    private enum Constant {
+        static let episodesTableName = "episodes"
+        static let mediaSourcesTableName = "media_sources"
+        static let mediaSourceIdColumnName = "mediaSourceId"
+        static let defaultMediaSourceName = "_default"
+        static let foreignKeyName = "fk_media_sources_id"
+    }
+
     private enum `Error`: Swift.Error {
         case missingDefaultSourceID
+        case notSQL
     }
 
     func prepare(on database: Database) async throws {
@@ -21,25 +30,44 @@ struct AddEpisodeMediaSourceRelation: AsyncMigration {
             mediaSource = first
         } else {
             let defaultMediaSource = MediaSource()
-            defaultMediaSource.name = "_default"
+            defaultMediaSource.name = Constant.defaultMediaSourceName
             try await defaultMediaSource.save(on: database)
             mediaSource = defaultMediaSource
         }
 
         guard let mediaSource else { throw Error.missingDefaultSourceID }
+        guard let sqlDatabase = database as? SQLDatabase else { throw Error.notSQL }
 
-        try await database.schema("episodes")
-            .field("mediaSourceId", .uuid, .references("media_sources", "id"))
+        try await database.schema(Constant.episodesTableName)
+            .field(FieldKey(stringLiteral: Constant.mediaSourceIdColumnName), .uuid)
             .update()
 
-        try await (database as! SQLDatabase)
-            .raw("UPDATE episodes SET mediaSourceId = \(bind: mediaSource.requireID())")
+        try await sqlDatabase
+            .raw("UPDATE \(raw: Constant.episodesTableName) SET \(raw: Constant.mediaSourceIdColumnName) = \(bind: mediaSource.requireID())")
             .run()
+
+        try await sqlDatabase
+            .raw("ALTER TABLE `\(raw: Constant.episodesTableName)` CHANGE `\(raw: Constant.mediaSourceIdColumnName)` `\(raw: Constant.mediaSourceIdColumnName)` varbinary(16) NOT NULL")
+            .run()
+
+        try await database.schema(Constant.episodesTableName)
+            .foreignKey(
+                FieldKey(stringLiteral: Constant.mediaSourceIdColumnName),
+                references: Constant.mediaSourcesTableName,
+                "id",
+                name: Constant.foreignKeyName
+            )
+            .update()
     }
 
     func revert(on database: Database) async throws {
-        try await database.schema("media_sources")
-            .deleteField("mediaSourceId")
+        try await database.schema(Constant.episodesTableName)
+            .deleteForeignKey(name: Constant.foreignKeyName)
             .update()
+
+        try await database.schema(Constant.episodesTableName)
+            .deleteField(FieldKey(stringLiteral: Constant.mediaSourceIdColumnName))
+            .update()
+
     }
 }
